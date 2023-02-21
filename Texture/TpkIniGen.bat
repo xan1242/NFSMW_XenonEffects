2f3b71f553d0110c20d8345e6768126908897e4b
xan1242 <8014093+xan1242@users.noreply.github.com>
2023-02-22 00:53:48 +0100

    re-introduce tpk generator

---
+++ Texture/TpkIniGen.bat
@@ -0,0 +1,49 @@
+@echo off
+:: TPK Generator for XNFSTPKTool
+set ININAME=XenonEffects.ini
+set FOLDERNAME=.
+IF NOT "%~1"=="" set FOLDERNAME="%1"
+IF NOT "%~2"=="" set ININAME="%2"
+
+<nul (set/p nlv=[TPK])> %ININAME%                              & ECHO(>> %ININAME%
+<nul (set/p nlv=Name = XENONEFFECTS)>> %ININAME%        & ECHO(>> %ININAME%
+<nul (set/p nlv=Version = 5)>> %ININAME%                       & ECHO(>> %ININAME%
+<nul (set/p nlv=Filename = XenonEffects.ini)>> %ININAME%    & ECHO(>> %ININAME%
+<nul (set/p nlv=FilenameHash = 0x)>> %ININAME%
+strhash XenonEffects.ini>> %ININAME%
+ECHO(>> %ININAME%
+<nul (set/p nlv=Animations = 0)>> %ININAME%
+ECHO(>> %ININAME%
+ECHO(>> %ININAME%
+FOR /R %FOLDERNAME% %%T IN (*.dds) DO (
+<nul (set/p nlv=[)>> %ININAME%
+strhash %%~nT>> %ININAME%
+<nul (set/p nlv=])>> %ININAME%                                 & ECHO(>> %ININAME%
+<nul (set/p nlv=File = %%~dpnT.dds)>> %ININAME%                & ECHO(>> %ININAME%
+<nul (set/p nlv=Name = %%~nT)>> %ININAME%                      & ECHO(>> %ININAME%
+<nul (set/p nlv=ClassNameHash = 0x6B6D324C)>> %ININAME%          & ECHO(>> %ININAME%
+<nul (set/p nlv=ImageCompressionType = 0x20)>> %ININAME%       & ECHO(>> %ININAME%
+<nul (set/p nlv=PaletteCompressionType = 0x0)>> %ININAME%      & ECHO(>> %ININAME%
+<nul (set/p nlv=NumPaletteEntries = 0)>> %ININAME%             & ECHO(>> %ININAME%
+<nul (set/p nlv=TilableUV = 0)>> %ININAME%                     & ECHO(>> %ININAME%
+<nul (set/p nlv=BiasLevel = 0)>> %ININAME%                     & ECHO(>> %ININAME%
+<nul (set/p nlv=RenderingOrder = 0)>> %ININAME%                & ECHO(>> %ININAME%
+<nul (set/p nlv=ScrollType = 0)>> %ININAME%                    & ECHO(>> %ININAME%
+<nul (set/p nlv=UsedFlag = 0)>> %ININAME%                      & ECHO(>> %ININAME%
+<nul (set/p nlv=ApplyAlphaSorting = 0)>> %ININAME%             & ECHO(>> %ININAME%
+<nul (set/p nlv=AlphaUsageType = 2)>> %ININAME%                & ECHO(>> %ININAME%
+<nul (set/p nlv=AlphaBlendType = 2)>> %ININAME%                & ECHO(>> %ININAME%
+<nul (set/p nlv=Flags = 0x0)>> %ININAME%                       & ECHO(>> %ININAME%
+<nul (set/p nlv=ScrollTimeStep = 0)>> %ININAME%                & ECHO(>> %ININAME%
+<nul (set/p nlv=ScrollSpeedS = 0)>> %ININAME%                  & ECHO(>> %ININAME%
+<nul (set/p nlv=ScrollSpeedT = 0)>> %ININAME%                  & ECHO(>> %ININAME%
+<nul (set/p nlv=OffsetS = 0)>> %ININAME%                       & ECHO(>> %ININAME%
+<nul (set/p nlv=OffsetT = 256)>> %ININAME%                     & ECHO(>> %ININAME%
+<nul (set/p nlv=ScaleS = 256)>> %ININAME%                      & ECHO(>> %ININAME%
+<nul (set/p nlv=ScaleT = 0)>> %ININAME%                        & ECHO(>> %ININAME%
+<nul (set/p nlv=PixelFormatUnk1 = 0x1)>> %ININAME%             & ECHO(>> %ININAME%
+<nul (set/p nlv=PixelFormatUnk2 = 0x5)>> %ININAME%             & ECHO(>> %ININAME%
+<nul (set/p nlv=PixelFormatUnk3 = 0x6)>> %ININAME%             & ECHO(>> %ININAME%
+ECHO(>> %ININAME%
+)
+EXIT /B 0
